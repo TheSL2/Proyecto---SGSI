d5cf5d02d8b63c12adcb255c6d5941a913b7405a
TheSL2 <omarsanluis50@gmail.com>
2026-08-06 21:03:21 -0600

    Fix | Adición de importaciones en Modelo ChecklistAuditoria

--- backend/app/Models/ChecklistAuditoria.php
+++ backend/app/Models/ChecklistAuditoria.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\RequisitoIso;
+use App\Models\Auditoria;
 
 class ChecklistAuditoria extends Model
 {
